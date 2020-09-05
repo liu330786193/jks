05c7d2cd94b36db97eb085dccd5a1c081c518f1e
liu330786193 <liu330786193@163.com>
2020-09-06 03:16:32 +0800

    first commit

---
+++ src/main/java/com/lyl/jks/JksApplication.java
@@ -0,0 +1,13 @@
+package com.lyl.jks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JksApplication.class, args);
+    }
+
+}
