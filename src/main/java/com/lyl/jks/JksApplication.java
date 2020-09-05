477becacdf7f00672623427dcaece6ec90858553
liu330786193 <liu330786193@163.com>
2020-09-06 03:22:11 +0800

    第一次提交

--- src/main/java/com/lyl/jks/JksApplication.java
+++ src/main/java/com/lyl/jks/JksApplication.java
@@ -2,12 +2,22 @@ package com.lyl.jks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JksApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JksApplication.class, args);
+    }
+
+    @ResponseBody
+    @GetMapping("/test/jks")
+    public Integer test(){
+        return 1;
     }
 
 }
